fb3defc2a0a42e4b2fe41004305938a7c3a8ead4
TusharRavaliya <130966020+TusharRavaliya@users.noreply.github.com>
2023-07-14 11:15:05 +0530

    Add files via upload

---
+++ prinf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()  
+{
+     printf( " My name is Tushar Ravaliya.\n");
+     printf( " My DOB is 31st May 2004.\n"); 
+     printf( " My Age is 19.\n");
+     printf( " My Address is Jamnagar.\n");
+     return 0;  
+}
